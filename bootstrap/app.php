002ae4413b6f0082fc8a311a129bbb37b4545dbd
Leonardo Vilarinho <leonardo-s@outlook.com>
2021-05-19 19:37:17 -0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
-
+header('Access-Control-Allow-Origin', '*');
+header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 /*
 |--------------------------------------------------------------------------
 | Create The Application
